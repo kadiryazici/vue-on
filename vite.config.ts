eff5f1dff59e6b97a789a53cb6691c6ef99e80bf
Kadir Yazıcı <kyzc411@outlook.com.tr>
2021-09-26 08:58:40 +0300

    feat: script to move types

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
          entry: resolve(__dirname, 'src/index.ts'),
          name: 'Vue-ON',
          formats: ['es', 'umd']
+      },
+      rollupOptions: {
+         external: ['@vue/runtime-dom', 'nanoid']
       }
    }
 });
