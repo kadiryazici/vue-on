5495db464d0ed33f0b94df14d20c957ab8322e0f
Kadir Yazıcı <kyzc411@outlook.com.tr>
2021-09-26 09:15:56 +0300

    feat: event as second parameter

--- src/events/onConnectionStatusChange.ts
+++ src/events/onConnectionStatusChange.ts
@@ -1,5 +1,5 @@
 export type OnlineOrOffline = 'online' | 'offline';
-export type OnlineOrOfflineEvent = RegisteredEvent<(kind: OnlineOrOffline) => any>;
+export type OnlineOrOfflineEvent = RegisteredEvent<(kind: OnlineOrOffline, event: Event) => any>;
 
 import { RegisteredEvent } from '../types';
 import { createID } from '../helpers/createID';
@@ -9,31 +9,35 @@ import { removeEvent } from '../helpers/removeChild';
 const registered = [] as OnlineOrOfflineEvent[];
 let isCreatedOnce = false;
 
+const add = window.addEventListener;
+const remove = window.removeEventListener;
+
 /**
  *@description a handler for online/offline events in browser.
  *@see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/Online_and_offline_events
  */
 export function onConnectionStatusChange(kind: OnlineOrOffline, handler: OnlineOrOfflineEvent['handler']) {
    if (!isCreatedOnce) {
-      window.addEventListener('online', onlineOfflineEventHandler);
-      window.addEventListener('offline', onlineOfflineEventHandler);
+      add('online', onlineOfflineEventHandler);
+      add('offline', onlineOfflineEventHandler);
       isCreatedOnce = true;
    }
+
    const event = { id: createID(), handler } as OnlineOrOfflineEvent;
    registered.push(event);
    onBeforeUnmount(() => {
       removeEvent(registered, event.id);
       if (registered.length <= 0) {
-         window.removeEventListener('online', onlineOfflineEventHandler);
-         window.removeEventListener('offline', onlineOfflineEventHandler);
+         remove('online', onlineOfflineEventHandler);
+         remove('offline', onlineOfflineEventHandler);
          isCreatedOnce = false;
       }
    });
 }
 
-function onlineOfflineEventHandler() {
+function onlineOfflineEventHandler(nativeEvent: Event) {
    const kind: OnlineOrOffline = navigator.onLine ? 'online' : 'offline';
    for (const event of registered) {
-      event.handler(kind);
+      event.handler(kind, nativeEvent);
    }
 }
